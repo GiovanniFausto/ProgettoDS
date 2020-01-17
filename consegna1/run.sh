f527ed4bd8dc707b864d2f8425ce1cc159e42938
GiovanniFausto <giovanni.f.95@hotmail.it>
2020-01-17 19:01:07 +0100

    Update run.sh

--- consegna1/run.sh
+++ consegna1/run.sh
@@ -1,10 +1,4 @@
-cd API
-docker build -t api-prod:v1 . -f Dockerfile-prod 
-cd .. 
-cd VMS
-docker build -t vms-prod:v1 . -f Dockerfile-prod 
-cd .. 
-cd VPS
-docker build -t vps-prod:v1 . -f Dockerfile-prod 
-cd ..
+docker build -t api-prod:v1 ./API -f API/Dockerfile-prod 
+docker build -t vms-prod:v1 ./VMS -f VMS/Dockerfile-prod 
+docker build -t vps-prod:v1 ./VPS -f VPS/Dockerfile-prod 
 docker-compose -f docker-compose-prod.yml up 
